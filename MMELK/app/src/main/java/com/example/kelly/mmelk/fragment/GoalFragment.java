9596ebf824fff147509bd5d1d9924f2fb657d585
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-12-03 01:23:59 -0600

    Feat: added layout files for MainActivity

--- MMELK/app/src/main/java/com/example/kelly/mmelk/fragment/GoalFragment.java
+++ MMELK/app/src/main/java/com/example/kelly/mmelk/fragment/GoalFragment.java
@@ -2,9 +2,17 @@ package com.example.kelly.mmelk.fragment;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.kelly.mmelk.R;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by Mahesh Gaya on 12/2/16.
@@ -13,6 +21,20 @@ import android.support.v4.content.Loader;
 public class GoalFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>{
     public GoalFragment(){
         //required default constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_goals, container, false);
+        ButterKnife.bind(this, rootView);
+        return rootView;
     }
 
     @Override
--- MMELK/app/src/main/java/com/example/kelly/mmelk/fragment/PointFragment.java
+++ MMELK/app/src/main/java/com/example/kelly/mmelk/fragment/PointFragment.java
@@ -2,9 +2,19 @@ package com.example.kelly.mmelk.fragment;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.kelly.mmelk.R;
+
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Mahesh Gaya on 12/2/16.
@@ -14,6 +24,21 @@ public class PointFragment extends Fragment implements LoaderManager.LoaderCallb
     public PointFragment(){
         //required default constructor
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_points, container, false);
+        ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
